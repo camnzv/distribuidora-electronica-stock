7e7a83d36e6c2bfc05ca2b848a8f5ddc7efe709b
camilaVillasboa <camvillasboa.ikigai@gmail.com>
2026-06-03 12:20:17 -0300

    Implementa Login de usuarios mediante base de datos usando SP_LOGIN_USUARIO

---
+++ DistribuidoraElectronicaStock/DistribuidoraElectronicaStock.DAL/UsuarioDAL.cs
@@ -0,0 +1,54 @@
+﻿using DistribuidoraElectronicaStock.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistribuidoraElectronicaStock.DAL
+{
+    public class UsuarioDAL
+    {
+        private Conexion conexion = new Conexion();
+
+        public Usuario ObtenerPorDniYPassword(int dni, string password)
+        {
+            SqlParameter[] parametros =
+            {
+                conexion.crearParametro("@dni", dni),
+                conexion.crearParametro("@password", password)
+            };
+
+            DataTable tabla =
+                conexion.LeerPorStoreProcedure(
+                    "SP_LOGIN_USUARIO",
+                    parametros);
+
+            if (tabla.Rows.Count == 0)
+                return null;
+
+            DataRow fila = tabla.Rows[0];
+
+            Rol rol = new Rol(
+                Convert.ToInt32(fila["rol_id"]),
+                fila["nombreRol"].ToString(),
+                fila["descripcion"].ToString()
+            );
+
+            return new Usuario(
+                Convert.ToInt32(fila["id_usuario"]),
+                fila["nombre"].ToString(),
+                fila["apellido"].ToString(),
+                fila["email"].ToString(),
+                fila["password"].ToString(),
+                Convert.ToInt32(fila["dni"]),
+                rol
+            );
+        }
+    }
+
+
+}
+
