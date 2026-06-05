7ea3c3d0300d030fcb39de24027c2b7fdc87f2ff
Agustin <agustinmtalco@gmail.com>
2026-06-05 01:25:12 -0300

    se agrego la conexion con la base de datos de las pantallas de gerente

---
+++ DistribuidoraElectronicaStock/DistribuidoraElectronicaStock.BBL/GestorReporteStock.cs
@@ -0,0 +1,30 @@
+﻿using DistribuidoraElectronicaStock.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace DistribuidoraElectronicaStock.BBL
+{
+    public class GestorReporteStock
+    {
+        private ReporteStockDAL reporteStockDAL;
+
+        public GestorReporteStock()
+        {
+            reporteStockDAL = new ReporteStockDAL();
+        }
+
+        public DataTable ObtenerStock(int? categoriaId)
+        {
+            return reporteStockDAL.ObtenerStock(categoriaId);
+        }
+
+        public DataTable ObtenerCategorias()
+        {
+            return reporteStockDAL.ObtenerCategorias();
+        }
+    }
+}
