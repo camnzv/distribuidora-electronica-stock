b767a6eb81fa8762a592b99e4c8268ed8fda68d6
camilaVillasboa <camvillasboa.ikigai@gmail.com>
2026-06-25 00:35:40 -0300

    agrego excepciones iniciales

---
+++ DistribuidoraElectronicaStock/DistribuidoraElectronicaStock.BBL/Excepciones/UsuarioInactivoException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistribuidoraElectronicaStock.BBL.Excepciones
+{
+   
+    /// Se lanza cuando un usuario intenta iniciar sesión
+    /// pero su cuenta fue desactivada por el administrador.
+  
+    public class UsuarioInactivoException : Exception
+    {
+        public UsuarioInactivoException()
+            : base("El usuario se encuentra inactivo. Contacte al administrador.") { }
+
+        public UsuarioInactivoException(string mensaje)
+            : base(mensaje) { }
+    }
+}
