782607d9bcb0f71cf10b220d8f70d7a097f00c6f
tatiana <rominatatianakumichel@gmail.com>
2026-06-25 01:17:42 -0300

    Agrego la funcionalidad de venta

---
+++ DistribuidoraElectronicaStock/DistribuidoraElectronicaStock.Presentacion/FrmRegistrarVenta.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace DistribuidoraElectronicaStock.Presentacion
+{
+    partial class FrmRegistrarVenta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.cmbCliente = new System.Windows.Forms.ComboBox();
+            this.dtpFecha = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(12, 65);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(48, 16);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "Cliente";
+            // 
+            // cmbCliente
+            // 
+            this.cmbCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbCliente.FormattingEnabled = true;
+            this.cmbCliente.Location = new System.Drawing.Point(86, 57);
+            this.cmbCliente.Name = "cmbCliente";
+            this.cmbCliente.Size = new System.Drawing.Size(207, 24);
+            this.cmbCliente.TabIndex = 1;
+            // 
+            // dtpFecha
+            // 
+            this.dtpFecha.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpFecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpFecha.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFecha.Location = new System.Drawing.Point(466, 59);
+            this.dtpFecha.Name = "dtpFecha";
+            this.dtpFecha.Size = new System.Drawing.Size(200, 22);
+            this.dtpFecha.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(404, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Fecha";
+            // 
+            // FrmRegistrarVenta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtpFecha);
+            this.Controls.Add(this.cmbCliente);
+            this.Controls.Add(this.lbl1);
+            this.Name = "FrmRegistrarVenta";
+            this.Text = "FrmRegistarVenta";
+            this.Load += new System.EventHandler(this.FrmRegistarVenta_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.ComboBox cmbCliente;
+        private System.Windows.Forms.DateTimePicker dtpFecha;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ DistribuidoraElectronicaStock/DistribuidoraElectronicaStock.Presentacion/FrmRegistrarVenta.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DistribuidoraElectronicaStock.BBL;
+using DistribuidoraElectronicaStock.Entidades;
+
+namespace DistribuidoraElectronicaStock.Presentacion
+{
+    public partial class FrmRegistrarVenta : Form
+    {
+        private GestorClientes _gestorClientes;
+
+        public FrmRegistrarVenta()
+
+        {
+            InitializeComponent();
+
+            _gestorClientes = new GestorClientes();
+        }
+
+        private void FrmRegistarVenta_Load(object sender, EventArgs e)
+        {
+            CargarClientes();
+            dtpFecha.Value = DateTime.Now; // carga la fecha de hoy por defecto
+
+        }
+
+        private void CargarClientes() {
+
+            var clientes = _gestorClientes.ObtenerClientes();
+
+            cmbCliente.DataSource = clientes;
+            cmbCliente.DisplayMember = "RazonSocial"; //esto se ve en el combo
+            cmbCliente.ValueMember = "idCliente"; // valor que guarda
+        
+        }
+    }
+}
